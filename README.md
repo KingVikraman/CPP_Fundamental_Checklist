c3e0506283f475aeb4e1f5665757d12832e2faca
Raja Vikraman <rvikrama@u91z04s01.42kl.edu.my>
2025-06-18 20:30:50 +0800

    added and made all the nessesary files for the CPP00 exercise

--- README.md
+++
@@ -1,2 +0,0 @@
-# C-_Fundamental_Checklist
-This would be my knowledge and understanding of the basics in C++ programming language
---
+++ phonebook/main.cpp
@@ -0,0 +1,37 @@
+#include "phonebook.hpp"
+#include <iostream>
+#include <string>
+
+int main() {
+	PhoneBook phoneBook;
+	std::string command;
+
+	std::cout << "📞 Welcome to My Awesome PhoneBook!" << std::endl;
+	std::cout << "Commands: ADD, SEARCH, EXIT" << std::endl;
+
+	while (true) {
+		std::cout << "👉 Enter command: ";
+		std::getline(std::cin, command);
+
+		if (std::cin.eof()) {
+			std::cout << "\n👋 EOF detected. Exiting..." << std::endl;
+			break;
+		}
+
+		if (command == "ADD") {
+			phoneBook.addContact();
+		}
+		else if (command == "SEARCH") {
+			phoneBook.searchContact();
+		}
+		else if (command == "EXIT") {
+			std::cout << "👋 Goodbye!" << std::endl;
+			break;
+		}
+		else {
+			std::cout << "❌ Invalid command. Try ADD, SEARCH, or EXIT." << std::endl;
+		}
+	}
+
+	return 0;
+}
